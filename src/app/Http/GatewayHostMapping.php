fa7eab39fceea15c266f0599ca4ef4b3a634448c
Matheus-Lara <matheusodelara@gmail.com>
2022-10-18 19:17:43 -0300

    chore: adds stock-microservice

--- src/app/Http/GatewayHostMapping.php
+++ src/app/Http/GatewayHostMapping.php
@@ -5,7 +5,8 @@ namespace App\Http;
 class GatewayHostMapping {
 	public static function getMicroservicesAndPorts(): array {
 		return [
-			env('HOST_MACHINE_PRODUCTS') => env('HOST_MACHINE_PRODUCTS_PORT')
+			env('HOST_MACHINE_PRODUCTS') => env('HOST_MACHINE_PRODUCTS_PORT'),
+			env('HOST_MACHINE_STOCKS') => env('HOST_MACHINE_STOCKS_PORT')
 		];
 	}
 }
